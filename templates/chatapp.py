d1d40092a5fd5a92693f6f53feeb7f6a2c40994f
DollyG <dollsgoitse@yahoo.com>
2018-05-25 11:29:02 +0000

    more done

--- templates/chatapp.py
+++ templates/chatapp.py
@@ -3,7 +3,20 @@ from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
+#
+banned_words = [
+    'sucker'
+    'bom'
+    'duck'
+    ]
 messages = []
+
+
+# @app.route('rooms/add')
+# def add_room():
+#     roomname = request.form['roomname']
+#     rooms[roomname] = []
+#     return redirect(....)
 
 @app.route("/")
 def get_index():
@@ -20,10 +33,18 @@ def get_userpage(username):
     
 
     
-@app.route("/new", methods=["POST"]) 
-def add_message():
-    username = request.form['username']
+@app.route("/<username>/new", methods=["POST"]) 
+def add_message(username):
+    
     text= request.form['message']
+   
+   #replace banned words in message using list-python
+    words = text.split()
+    words = [ "*" * len(word) if word.lower() in banned_words else word for word in words]
+    
+    text = " ".join(map(str,words))
+        
+    
     
     message = {
         'sender': username,
@@ -33,4 +54,5 @@ def add_message():
     messages.append(message)
     return redirect(username)
 
-app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
